a80eeb5f9101cac9d79fc9947db78fb0f7b0d334
Rockthor <jan.carvajal1@u.icesi.edu.co>
2023-07-25 22:26:30 -0500

    Now it is possible to add reviews for the movies

--- Backend/movies/src/main/java/dev/carvi/movies/ReviewController.java
+++ Backend/movies/src/main/java/dev/carvi/movies/ReviewController.java
@@ -4,10 +4,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -18,6 +15,7 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @PostMapping
+    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
     }
--- Frontend/movie-gold-v1/src/App.js
+++ Frontend/movie-gold-v1/src/App.js
@@ -6,10 +6,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header'
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
 
@@ -28,6 +31,21 @@ function App() {
     getMovies();
   }, [])
 
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviewIds);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <Header/>
@@ -35,6 +53,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies} />}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews}  />}></Route>
         </Route>
       </Routes>
 
