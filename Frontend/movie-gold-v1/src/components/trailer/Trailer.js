7fbadfbca863b389bcb94f5a6a3ab4a530e6f273
Rockthor <jan.carvajal1@u.icesi.edu.co>
2023-07-25 20:17:56 -0500

    Trailer is already working

--- Frontend/movie-gold-v1/src/components/trailer/Trailer.js
+++ Frontend/movie-gold-v1/src/components/trailer/Trailer.js
@@ -4,11 +4,11 @@ import "./Trailer.css"
 
 const Trailer = () => {
 
-    let params = userParams();
+    let params = useParams();
     const key = params.ytTrailerId; 
 
   return (
-    <div className="ract-player-container">
+    <div className="react-player-container">
         {(key!=null)?<ReactPlayer controls="true" playing={true} url={`https://www.youtube.com/watch?v=${key}`}
         width='100%' height='100%'/>:null}
     </div>
